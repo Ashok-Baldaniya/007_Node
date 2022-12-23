b978947acc57c16d4c55ccb9d36c434bd4e19b8c
Ashok153 <baldaniyaashok153@gmail.com>
2022-12-23 09:18:33 +0530

    ok

---
+++ Assignment/Module_03/Q5/app.js
@@ -0,0 +1,14 @@
+const time=require("./time")
+const country = require("./country")
+// const geocode = require("./geocode")
+
+// time.getdata((result)=>{
+//     console.log(result);
+// })
+
+country.getcountry(()=>{
+    
+    console.log(result);
+})
+
+// console.log(country.getcountry());
